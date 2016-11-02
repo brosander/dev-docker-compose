f9f3ae1021565d08c7df4600b5e196f013f34097
Bryan Rosander <brosander@apache.org>
2016-11-02 14:46:46 -0400

    adding basedir check before rm -rf

--- nifi/cluster/pre1.0/unsecured/clean-all.sh
+++ nifi/cluster/pre1.0/unsecured/clean-all.sh
@@ -3,6 +3,10 @@
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in#answer-246128
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ -z "$BASE_DIR" ]; then
+  echo "Couldn't resolve basedir, exiting before scary rm -rf"
+fi
+
 rm -rf "$BASE_DIR/dev-dockerfiles"
 rm -rf "$BASE_DIR/ssh-key"
 "$BASE_DIR/clean.sh"
--- nifi/cluster/pre1.0/unsecured/clean.sh
+++ nifi/cluster/pre1.0/unsecured/clean.sh
@@ -3,4 +3,8 @@
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in#answer-246128
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ -z "$BASE_DIR" ]; then
+  echo "Couldn't resolve basedir, exiting before scary rm -rf"
+fi
+
 rm -rf "$BASE_DIR/target"
